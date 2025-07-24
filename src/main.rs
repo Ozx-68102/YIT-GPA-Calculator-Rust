b5b989e24311f4395afcd3ce81d988257ba123c5
Ozx-68102 <ozx1072960726@gmail.com>
2025-07-25 03:47:11 +0800

    增加完整打包功能和发布模式的编译配置

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
 use anyhow::{Context, Result};
+use axum::serve;
+use rust_embed::RustEmbed;
 use std::net::SocketAddr;
 use tera::Tera;
 use tokio::net::TcpListener;
 use webbrowser;
-use axum::serve;
 
 mod models;
 mod utils;
 mod web_scraping;
 mod web;
 
+// 使用 RustEmbed 宏来嵌入整个 templates 文件夹
+// folder 路径是相对于 Cargo.toml 文件的
+#[derive(RustEmbed)]
+#[folder = "templates/"]
+struct Asset;   // 虚拟结构体, 用于持有嵌入的模板文件
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // 初始化模板引擎
-    let tera = Tera::new("templates/**/*.html")?;
+    let mut tera = Tera::default();
+
+    // 遍历所有嵌入的文件
+    for file_path in Asset::iter() {
+        // 获取文件内容
+        if let Some(embedded_file) = Asset::get(&file_path) {
+            // embedded_file.data 是文件内容, 类型为 Vec<u8>
+            // embedded_file.metadata 是文件元数据, 比如说是否为目录
+            // 将 Vec<u8> 转换为 &str
+            let content = std::str::from_utf8(embedded_file.data.as_ref())?;
+
+            // 将 HTML 模板添加到 Tera 实例
+            // 这里的 content 已经是借用的形式了(&str), 因此可以不需要借用符号(&)
+            tera.add_raw_template(&file_path, content).with_context(|| format!("导入嵌入文件失败: {}", file_path))?;
+        }
+    }
+
+    // 构建 Tera 的继承链
+    tera.build_inheritance_chains().context("构建Tera继承链失败.")?;
 
     // 创建路由
     let app = web::create_router(tera);
